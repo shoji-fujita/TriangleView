d7da7c5b2c1009b7cd9820fc9c8676e21b20d67b
shoji-fujita <shoji.fujita.0819@gmail.com>
2016-12-31 12:44:58 +0900

    Initial Commit

---
+++ TriangleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TriangleView
+//
+//  Created by shoji on 2016/12/31.
+//  Copyright © 2016年 shoji fujita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
